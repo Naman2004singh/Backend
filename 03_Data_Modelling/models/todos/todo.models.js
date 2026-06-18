dccb3e401034cc8d1827f4e663a75189b1474a40
Naman2004singh <naman.2singh9@gmail.com>
2026-06-18 13:24:43 +0530

    created the Todo Model

--- 03_Data_Modelling/models/todos/todo.models.js
+++ 03_Data_Modelling/models/todos/todo.models.js
@@ -1,7 +1,25 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-    
+    content : {
+        type : String,
+        required  :true
+    },
+    complete : {
+        type : Boolean,
+        default : false
+    },
+    createdBy : {
+        // it is related with the user schema
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    subTodos : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "SubTodo"
+        }
+    ]   // array of objects (multiple objects) - subTodos
 }, {timestamps : true})
 
 export const Todo = mongoose.model("Todo", todoSchema)
